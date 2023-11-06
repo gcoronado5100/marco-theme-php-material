b91746a4994de42e6b276dc04012c4016e2d4b1c
Marco Cordero <115248206+Marcolamb@users.noreply.github.com>
2023-11-06 17:02:31 -0500

    se anadieron los links de las hojas de estilos faltantes y de las de js.

--- includes/admin/mc-enqueue.php
+++ includes/admin/mc-enqueue.php
@@ -8,8 +8,22 @@
 function mc_enqueue_cb()
 {
     // CSS
-    wp_enqueue_style('mc-theme-mdb-min', MC_THEME_URI . '/assets/css/mdb.min.css', [], null, 'all');
+    // Enqueue Font Awesome CSS
+    wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v5.11.2/css/all.css', [], null, 'all');
+
+    // Enqueue Google Fonts Roboto CSS
+    wp_enqueue_style('google-fonts-roboto', 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap', [], null, 'all');
+
+    // Enqueue mdb-min
+    wp_enqueue_style('mc-theme-mdb-min',MC_THEME_URI . '/assets/css/mdb.min.css', [], null, 'all');
+
+    // Enqueue Custom CSS
+    wp_enqueue_style('mc-theme-custom-style', MC_THEME_URI . '/assets/css/style.css', [], null, 'all');
 
     // JS
+    // Enqueue MDB JS
     wp_enqueue_script('mc-theme-mdb-js', MC_THEME_URI . '/assets/js/mdb.min.js', [], null, true);
+
+    // Enqueue Custom
+    wp_enqueue_script('mc-theme-custom-script', MC_THEME_URI . '/assets/js/script.js', [], null, true);
 }
